8b6941cc8b2574637f6d4f16c0750371e7e05fbf
Yeray Callero <yeray@geosophic.com>
2015-01-25 04:36:34 +0100

    Work in progress.

--- examples/smartshop-mock/keen.dashboard.js
+++ examples/smartshop-mock/keen.dashboard.js
@@ -3,289 +3,241 @@ var client = new Keen({
   readKey: "92091663f85279d2535672a51c1a6c3cfdfdf509940c0c8acb3514ab6a43c1389de2cee1c21d8713c10a8b96d81f033d8492b70e1166b0e909ee2876598cdf7cce8779cd374b0a6c755f3e83c7e9de293be834441692d3f577339cea3ae3b3b941e0b851db04cd5bcea1f4992ddc581b"
 });
 
-var geoProject = new Keen({
-  projectId: "5465e8423831440585aae8b9",
-  readKey: "92091663f85279d2535672a51c1a6c3cfdfdf509940c0c8acb3514ab6a43c1389de2cee1c21d8713c10a8b96d81f033d8492b70e1166b0e909ee2876598cdf7cce8779cd374b0a6c755f3e83c7e9de293be834441692d3f577339cea3ae3b3b941e0b851db04cd5bcea1f4992ddc581b"
-});
+var mapboxAccount = {
+	apiAccessToken: "pk.eyJ1IjoieWVyYXljYWxsZXJvIiwiYSI6Il9SUFdSMG8ifQ.z94oPq1u1aXBemi5UImfjw",
+	mapId: "yeraycallero.l1fah773"
+}; 
 
 Keen.ready(function(){
+   showDailyActivityChart();
+   showClientGenderChart();
+   showClientAgeChart();
+   showOfferShownByBrandChart();
+   showOfferShownByCategoryChart();
+   showActivityMap();
+});
 
-  // ----------------------------------------
-  // Mapbox Demo
-  // ----------------------------------------
-  var DEFAULTS = {
-    coordinates: {
-      lat: 37.77350,
-      lng: -122.41104
-    },
-    zoom: 15
-  };
+//------------- Offer shown by category chart --------
+function showOfferShownByCategoryChart() {
+ 	var keenFilter = [];
+	keenFilter.push({
+		property_name: "type",
+		operator: "eq",
+		property_value: "OFFERSHOWN"
+	});
+	
+	var offers_count = new Keen.Query("count", {
+		eventCollection: "smartshop_events",
+		groupBy: "offer.category",
+		filters: keenFilter
+	});
+	
+	client.draw(offers_count, document.getElementById("offersShownByCategoryChart"), {
+		chartType: "piechart",
+		titlePosition: 'none',
+		height: "auto",
+		width: "auto",
+		colors: null,
+		chartOptions: {
+			is3D: true,			
+		}
+	});	
+}
 
-  var map,
-      markerStart = DEFAULTS.coordinates,
-      activeMapData;
+//------------- Offer shown by brand chart --------
+function showOfferShownByBrandChart() {
+ 	var keenFilter = [];
+	keenFilter.push({
+		property_name: "type",
+		operator: "eq",
+		property_value: "OFFERSHOWN"
+	});
+	
+	var offers_count = new Keen.Query("count", {
+		eventCollection: "smartshop_events",
+		groupBy: "offer.brand.name",
+		filters: keenFilter
+	});
+	
+	client.draw(offers_count, document.getElementById("offersShownByBrandChart"), {
+		chartType: "piechart",
+		titlePosition: 'none',
+		height: "auto",
+		width: "auto",
+		colors: null,
+		chartOptions: {
+			is3D: true,
+			legend: 'none'
+		}
+	});	
+}
 
+//------------- Client age chart ------------------
+function showClientAgeChart() {
+	var keenFilter = [];
+	keenFilter.push({
+		property_name: "client.ageRange",
+		operator: "exists",
+		property_value: true
+	});
+	
+	var gender_count = new Keen.Query("count_unique", {
+		eventCollection: "smartshop_events",
+		targetProperty: "client.id",
+		groupBy: "client.ageRange",
+		filters: keenFilter
+	});	
+	
+	client.draw(gender_count, document.getElementById("clientAgeChart"), {
+		chartType: "piechart",
+		titlePosition: 'none',
+		height: "auto",
+		width: "auto",
+		colors: null,
+		chartOptions: {
+			is3D: true
+		}
+	});	
+}
 
-    L.mapbox.accessToken = "pk.eyJ1Ijoia2Vlbi1pbyIsImEiOiIza0xnNXBZIn0.PgzKlxBmYkOq6jBGErpqOg";
-    map = L.mapbox.map("map", "keen-io.kae20cg0", {
-      attributionControl: true,
-      center: [markerStart.lat, markerStart.lng],
-      zoom: DEFAULTS.zoom
+//------------- Client gender chart ---------------
+function showClientGenderChart() {
+	var keenFilter = [];
+	keenFilter.push({
+		property_name: "client.gender",
+		operator: "exists",
+		property_value: true
+	});
+	
+	var gender_count = new Keen.Query("count_unique", {
+		eventCollection: "smartshop_events",
+		targetProperty: "client.id",
+		groupBy: "client.gender",
+		filters: keenFilter
+	});	
+	
+	client.draw(gender_count, document.getElementById("clientGenderChart"), {
+		chartType: "piechart",
+		titlePosition: 'none',
+		height: "auto",
+		width: "auto",
+		colors: null,
+		chartOptions: {
+			is3D: true
+		}
+	});
+}
+
+//------------- Daily activity chart --------------
+
+function showDailyActivityChart() {
+	var keenFilter = [];
+	keenFilter.push({
+		property_name: "type",
+		operator: "eq",
+		property_value: "SERVICESTART"
+	});
+    
+	var activity_timeline = new Keen.Query("count", {
+		eventCollection: "smartshop_events",
+		interval: "daily",
+		timeframe: {
+			start: "2014-09-01T00:00:00.000Z",
+			end: "2014-10-01T00:00:00.000Z"
+		},
+		filters: keenFilter
+	});
+  
+	client.draw(activity_timeline, document.getElementById("dailyActivityChart"), {
+		chartType: "linechart",
+		titlePosition: 'none',
+		height: "auto",
+		width: "auto",
+		colors: null
+	});
+}
+
+//------------- Activity map ----------------------
+
+function showActivityMap() {
+	var mallData = {
+		ID: "1",
+		Name: "Centro Comercial XXXX"
+	};
+	
+	initializeMap(mallData, map).done(function(map) {
+		showActivityMarkersInMap(map);			
+	});
+}
+
+function initializeMap(mallData, map) {
+	var INITIAL_SETTINGS = {
+		coordinates: {
+			lat: 0,
+			lng: 0
+		},
+		zoom: 12
+	},
+	keenFilter = [],
+	deferred = $.Deferred();
+	
+	L.mapbox.accessToken = mapboxAccount.apiAccessToken;
+    
+    keenFilter.push({
+    	property_name: "mall.id",
+    	operator: "eq",
+    	property_value: mallData.ID
     });
-    var center = map.getCenter();
-    var zoom = map.getZoom();
+    
+    var mall_location = new Keen.Query("select_unique", {
+    	eventCollection: "smartshop_events",
+    	targetProperty: "mall.location",
+    	filters: keenFilter
+    });
+    
+    client.run(mall_location, function(err, res){
+    	var mallLocation = res.result[0];
+    	INITIAL_SETTINGS.coordinates.lat = mallLocation[1];
+    	INITIAL_SETTINGS.coordinates.lng = mallLocation[0];
+    	map = L.mapbox.map("map", mapboxAccount.mapId, {
+			attributionControl: true,
+			center: [INITIAL_SETTINGS.coordinates.lat, INITIAL_SETTINGS.coordinates.lng],
+			zoom: INITIAL_SETTINGS.zoom
+		});
+		var mallDataLayer = L.layerGroup().addTo(map);
+		L.marker(new L.LatLng(INITIAL_SETTINGS.coordinates.lat, INITIAL_SETTINGS.coordinates.lng), {
+          icon: L.mapbox.marker.icon({
+              "marker-color": '#000'
+            }),
+          title: mallData.Name
+        }).addTo(mallDataLayer);
+		deferred.resolve(map);
+    }); 
+    
+    return deferred.promise();   
+}
 
-    /*z = zoom-1;
-    if (zoom = 0){
-      radius = false;
-    }
-    else {
-      radius = 10000/Math.pow(2,z);
-    }*/
-
-    var geoFilter = [];
-    geoFilter.push({
+function showActivityMarkersInMap(map) {
+	
+    var keenFilter = [];
+    keenFilter.push({
       property_name: "type",
       operator: "eq",
       property_value: "SERVICESTART"
     });
-    /*geoFilter.push({
-      property_name : "keen.location.coordinates",
-      operator : "within",
-      property_value: {
-        coordinates: [center.lng, center.lat],
-        max_distance_miles: radius
-      }
-    });*/
 
-    /*var geoFilter2 = [];
-    geoFilter2.push({
-      property_name: "user.age",
-      operator: "gt",
-      property_value: "50"
-    });
-    geoFilter2.push({
-      property_name : "keen.location.coordinates",
-      operator : "within",
-      property_value: {
-        coordinates: [center.lng, center.lat],
-        max_distance_miles: radius
-      }
-    });*/
+	var heat = L.heatLayer([], {maxZoom: 15}).addTo(map);  
 
-    activeMapData = L.layerGroup().addTo(map);
-
-    //map.attributionControl.addAttribution('<a href="https://keen.io/">Custom Analytics by Keen IO</a>');
-
-    var scoped_events = new Keen.Query("select_unique", {
+    var activity_events = new Keen.Query("select_unique", {
       eventCollection: "smartshop_events",
       targetProperty: "keen.location.coordinates",
-      filters: geoFilter
+      filters: keenFilter
     });
-    geoProject.run(scoped_events, function(err, res){
-      console.log("events", res);
-
-      Keen.utils.each(res.result, function(coord, index){
-        var em = L.marker(new L.LatLng(coord[1], coord[0]), {
-          icon: L.mapbox.marker.icon({
-              "marker-color": Keen.Dataviz.defaults.colors[0]
-            })
-        }).addTo(activeMapData);
+    
+    client.run(activity_events, function(err, res){
+      Keen.utils.each(res.result, function(coord, index){        
+       	heat.addLatLng(new L.LatLng(coord[1], coord[0]));
       });
     });
+}
 
-    /*var scoped_events_2 = new Keen.Query("select_unique", {
-      eventCollection: "status_update",
-      targetProperty: "keen.location.coordinates",
-      filters: geoFilter2
-    });
-    geoProject.run(scoped_events_2, function(err, res){
-      console.log("events", res);
-
-      Keen.utils.each(res.result, function(coord, index){
-        var em = L.marker(new L.LatLng(coord[1], coord[0]), {
-          icon: L.mapbox.marker.icon({
-              "marker-color": Keen.Dataviz.defaults.colors[1]
-            })
-        }).addTo(activeMapData);
-      });
-    });*/
-
-    map.on('zoomend', function(e) {
-      resize();
-    });
-    map.on('dragend', function(e) {;
-      resize();
-    });
-
-
-
-  var resize = function(){
-    //activeMapData.clearLayers();
-
-    center = map.getCenter(),
-    zoom = map.getZoom();
-
-    //z = zoom-1;
-    //if (zoom = 0){
-    //  radius = false;
-    //}
-    //else {
-    //  radius = 10000/Math.pow(2,z);
-    //}
-    /*geoFilter.pop();
-    geoFilter.push({
-      property_name : "keen.location.coordinates",
-      operator : "within",
-      property_value: {
-        coordinates: [center.lng, center.lat],
-        max_distance_miles: radius
-      }
-    });*/
-
-   /* geoFilter2.pop();
-    geoFilter2.push({
-      property_name : "keen.location.coordinates",
-      operator : "within",
-      property_value: {
-        coordinates: [center.lng, center.lat],
-        max_distance_miles: radius
-      }
-    });*/
-
-
-    /*var scoped_events_3 = new Keen.Query("select_unique", {
-      eventCollection: "status_update",
-      targetProperty: "keen.location.coordinates",
-      filters: geoFilter
-    });
-    geoProject.run(scoped_events_3, function(err, res){
-      console.log("events", res);
-
-      Keen.utils.each(res.result, function(coord, index){
-        var em = L.marker(new L.LatLng(coord[1], coord[0]), {
-          icon: L.mapbox.marker.icon({
-              "marker-color": Keen.Dataviz.defaults.colors[0]
-            })
-        }).addTo(activeMapData);
-      });
-    });*/
-
-    /*var scoped_events_4 = new Keen.Query("select_unique", {
-      eventCollection: "status_update",
-      targetProperty: "keen.location.coordinates",
-      filters: geoFilter2
-    });
-    geoProject.run(scoped_events_4, function(err, res){
-      console.log("events", res);
-
-      Keen.utils.each(res.result, function(coord, index){
-        var em = L.marker(new L.LatLng(coord[1], coord[0]), {
-          icon: L.mapbox.marker.icon({
-              "marker-color": Keen.Dataviz.defaults.colors[1]
-            })
-        }).addTo(activeMapData);
-      });
-    });*/
-  };
-
-
-
-  // ----------------------------------------
-  // Violations line chart
-  // ----------------------------------------
-  var pageviews_timeline = new Keen.Query("count", {
-    eventCollection: "pageviews",
-    interval: "hourly",
-    groupBy: "user.device_info.browser.family",
-    timeframe: {
-      start: "2014-05-04T00:00:00.000Z",
-      end: "2014-05-05T00:00:00.000Z"
-    }
-  });
-  client.draw(pageviews_timeline, document.getElementById("chart-01"), {
-    chartType: "linechart",
-    title: null,
-    height: 175,
-    width: "auto",
-    colors: null,
-    chartOptions: {
-      legend: { position: "none" },
-      tooltip: { trigger: 'none' },
-      chartArea: {
-        left: "5%",
-        top: "5%",
-        height: "85%",
-        width: "95%"
-      }
-    }
-  });
-
-
-  // ----------------------------------------
-  // Violations
-  // ----------------------------------------
-  var impressions_timeline = new Keen.Query("count", {
-    eventCollection: "impressions",
-    groupBy: "ad.advertiser",
-    interval: "hourly",
-    timeframe: {
-      start: "2014-05-04T00:00:00.000Z",
-      end: "2014-05-05T00:00:00.000Z"
-    }
-  });
-  client.draw(impressions_timeline, document.getElementById("chart-03"), {
-    chartType: "columnchart",
-    title: false,
-    height: 175,
-    width: "auto",
-    chartOptions: {
-      legend: { position: "none" },
-      tooltip: { trigger: 'none' },
-      chartArea: {
-        left: "5%",
-        top: "5%",
-        height: "85%",
-        width: "93%"
-      },
-      bar: {
-        groupWidth: "85%"
-      },
-      isStacked: true
-    }
-  });
-
-  // ----------------------------------------
-  // Violations by Officer
-  // ----------------------------------------
-  var impressions_timeline_by_country = new Keen.Query("count", {
-    eventCollection: "pageviews",
-    interval: "hourly",
-    timeframe: {
-      start: "2014-04-30T12:00:00.000Z",
-      end: "2014-05-05T00:00:00.000Z"
-    }
-  });
-  client.draw(impressions_timeline_by_country, document.getElementById("chart-05"), {
-    chartType: "columnchart",
-    title: false,
-    height: 175,
-    width: "auto",
-    chartOptions: {
-      legend: { position: "none" },
-      tooltip: { trigger: 'none' },
-      chartArea: {
-        left: "5%",
-        top: "5%",
-        height: "85%",
-        width: "93%"
-      },
-      bar: {
-        groupWidth: "85%"
-      },
-      isStacked: true
-    }
-  });
-
-
-});
